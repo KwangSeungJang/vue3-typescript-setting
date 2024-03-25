18d0e8eb89c6409e3f88643eeda09d77e6320597
jang <lemona9201@gmail.com>
2024-03-25 16:22:23 +0900

    project init

---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="page">
+        Index페이지
+        <BasicLayout>
+            <div class="page__container">
+                <section class="page__container__navigation"></section>
+                <section class="page__container__notice"></section>
+            </div>
+        </BasicLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@components/atoms/layout/BasicLayout.vue';
+</script>
+
+<style lang="scss" scoped>
+    
+.page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 100%;
+
+    background-color: $color-black-900;
+}
+
+</style>
