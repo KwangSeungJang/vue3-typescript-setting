0490b2af6caa0680f33e3a9551b65b53950d8f0f
jang <lemona9201@gmail.com>
2024-03-25 16:43:37 +0900

    first commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,7 +20,6 @@ import BasicLayout from '@components/atoms/layout/BasicLayout.vue';
     display: flex;
     align-items: center;
     justify-content: center;
-
     width: 100%;
     height: 100%;
 
